87127968d72707c4f3c48de70f882fdd952a7d34
Austin Heiman <atheimanksu@gmail.com>
2015-01-07 09:34:20 -0600

    making pip installable via git

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@
 
 from distutils.core import setup
 
- setup(name='django-reviews',
-       version='0.1',
-       description='Django abstract base classes for creating a reviews application',
-       author='Austin Heiman',
-       author_email='atheimanksu@gmail.com',
-       url='https://github.com/atheiman/django-reviews',
+setup(
+    name='django-reviews',
+    version='0.1',
+    description='Django abstract base classes for creating a reviews application',
+    author='Austin Heiman',
+    author_email='atheimanksu@gmail.com',
+    url='https://github.com/atheiman/django-reviews',
 )
