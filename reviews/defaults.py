87411398454a0fa6f931510aa60b28b627c49777
Austin Heiman <atheimanksu@gmail.com>
2015-01-12 01:25:54 -0600

    checking that MAX_SCORE > MIN_SCORE.

--- reviews/defaults.py
+++ reviews/defaults.py
@@ -1,4 +1,7 @@
 from django.conf import settings
+from django.core.checks import register, Error
+
+
 
 DJANGO_REVIEWS = getattr(settings, "DJANGO_REVIEWS", {})
 
@@ -13,3 +16,19 @@ UPDATED_COMPARISON_SECONDS = DJANGO_REVIEWS.get('UPDATED_COMPARISON_SECONDS', 10
 AVG_SCORE_DIGITS = DJANGO_REVIEWS.get('AVG_SCORE_DIGITS', 2)
 COMMENT_REQUIRED = DJANGO_REVIEWS.get('COMMENT_REQUIRED', False)
 COMMENT_APPROVAL_REQUIRED = DJANGO_REVIEWS.get('COMMENT_APPROVAL_REQUIRED', False)
+
+
+
+@register('reviews')
+def example_check(app_configs, **kwargs):
+    errors = []
+    if MAX_SCORE <= MIN_SCORE:
+        errors.append(
+            Error(
+                'DJANGO_REVIEWS settings error: MAX_SCORE less than or equal to MIN_SCORE',
+                'Adjust settings.DJANGO_REVIEWS dict so that MAX_SCORE > MIN_SCORE',
+                obj=settings,
+                id='reviews.E001',
+            )
+        )
+    return errors
