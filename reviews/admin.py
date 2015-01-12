149e1887e035774e856d06da85e105fe4850a725
Austin Heiman <atheimanksu@gmail.com>
2015-01-12 00:53:40 -0600

    added admin filter to sort by reviewed model

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,13 +1,15 @@
+from django.contrib.contenttypes.models import ContentType
 from django.contrib import admin
 from django.core.urlresolvers import reverse
+from django.db.models import get_models
 from .models import Review
 
 
 
 def model_name(obj):
-    """Return name of related model."""
+    """Return name of reviewed model."""
     return obj.content_type.name.title()
-model_name.short_description = "Related model"
+model_name.short_description = "Reviewed model"
 
 
 
@@ -27,6 +29,31 @@ reviewed_object_linked.short_description = "Reviewed object"
 
 
 
+class ReviewedModelListFilter(admin.SimpleListFilter):
+    title = ('Reviewed model')
+    parameter_name = 'reviewed_model'
+
+    def lookups(self, request, model_admin):
+        models = get_models(include_auto_created=True)
+
+        reviewable_models = []
+        for model in models:
+            if hasattr(model, 'reviews'):
+                reviewable_models.append(
+                    (
+                        ContentType.objects.get_for_model(model).name,
+                        ContentType.objects.get_for_model(model).name.title(),
+                    )
+                )
+
+        return reviewable_models
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            return queryset.filter(content_type__name=self.value())
+
+
+
 class ReviewAdmin(admin.ModelAdmin):
     list_display = [
         'id',
@@ -38,6 +65,7 @@ class ReviewAdmin(admin.ModelAdmin):
         'comment_approved',
     ]
     list_filter = [
+        ReviewedModelListFilter,
         'comment_approved',
         'score',
     ]
